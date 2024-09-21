479c84728475876d727b9a84234d2beddc18a6a9
itzmietek <szymonszymon0q1@gmail.com>
2024-09-21 13:22:56 +0200

    Update GuesserGame.cs

--- C#/GuesserGame.cs
+++ C#/GuesserGame.cs
@@ -1,1 +1,25 @@
+using System; 
 
+public class Program 
+{
+    public static void Main(string[] args) 
+    {
+        Random random = new Random();
+        int n = random.Next(1, 10);
+        Console.WriteLine("guess the drawn number between 1 and 10");
+        int t = int.Parse(Console.ReadLine());
+        if (t == n)
+        {
+            Console.WriteLine("you guessed correctly");
+        }
+        else if (t>10)
+        {
+            Console.WriteLine("Number is bigger than 10");
+        }
+        else
+        {
+            Console.WriteLine("you guessed wrong");
+        }
+        Console.WriteLine("Random number were : " + n);
+    }
+}
